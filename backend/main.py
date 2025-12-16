d1444e828e3c03032da123ecf0a113107f11fa12
Zaine2k <165634822+Zaine2k@users.noreply.github.com>
2025-12-16 00:56:50 -0700

    Update origins

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,9 @@ app = FastAPI(title="Calgary Housing Price API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://housingpredictor2.vercel.app"
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
